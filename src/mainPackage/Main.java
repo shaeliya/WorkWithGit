3c1686b1680796f5ae26161c7f89f1078d831a39
shalhevet2001@gmail.com <shalhevet2001@gmail.com>
2021-03-08 11:29:12 +0200

    main changese

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -1,3 +1,11 @@
+//========================
+//Shalhevet Eliyahu 
+//ID:211661160
+//shalhevet2001@gmail.com
+//Maya Pasha
+//322290180
+//mayapasha56@gmail.com
+//=========================
 package mainPackage;
 import primitives.*;
 import static java.lang.System.out;
