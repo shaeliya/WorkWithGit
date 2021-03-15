769365c3778c2f136b06eaf6cc682e0dd3cac682
shalhevet2001@gmail.com <shalhevet2001@gmail.com>
2021-03-15 19:04:58 +0200

    test on triangle plane sphere

--- src/unittests/TriangleTests.java
+++ src/unittests/TriangleTests.java
@@ -13,9 +13,13 @@ package unittests;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
 import org.junit.Test;
 import  primitives.Point3D;
-import  geometries.Triangle;
+import primitives.Ray;
+import primitives.Vector;
+import  geometries.*;
 /**
  * @author shalh
  *
@@ -38,8 +42,61 @@ public class TriangleTests {
 		//Test if the normal of the triangle is normalized
 		
 		assertEquals("Test fails, the normal of the triangle is not normalized",1,triangle.getNormal(point1).length(),0.000001);
-
+			
+		}
 	
+	/**
+	 * Test method for {@link geometries.Triangle#findIntsersections(Ray ray)}.
+	 */
+	@Test		
+	public void testFindIntsersections() {
+		Point3D point1= new Point3D(1,0,0);
+		Point3D point2= new Point3D(0,1,0);
+		Point3D point3= new Point3D(1,1,0);
+		Vector vector1= new Vector(0,0,-1);
+		Point3D point4= new Point3D(0.5,0.75,1);
+		Point3D point5= new Point3D(0.25,0.25,1);
+		Point3D point6= new Point3D(1.25,1.25,1);
+		Point3D point7= new Point3D(0.5,0.5,1);
+		Point3D point8= new Point3D(1,1,1);
+		Point3D point9= new Point3D(1,5,1);
+		Triangle triangle=new Triangle(point1,point2,point3);		
+	    // ============ Equivalence Partitions Tests ==============
+		//Ray inside polygon/triangle
+		Ray ray1 = new Ray (point4,vector1);
+		List <Point3D>point3DList1 = triangle.findIntsersections(ray1);
+		assertEquals("The test failed, the function found more than one intersection point",1,point3DList1.size(),0.1);
+		assertTrue("The test failed, the function did not find what it was looking for",point3DList1.get(0).equals(new Point3D(0.5,0.75,0)));
+		// ============ Equivalence Partitions Tests ==============
+		//Ray outside against edge
+		Ray ray2 = new Ray (point5,vector1);
+		List <Point3D>point3DList2 = triangle.findIntsersections(ray2);
+		assertEquals("The test failed, the function found more than one intersection point",1,point3DList2.size(),0.1);
+		assertTrue("The test failed, the function did not find what it was looking for",point3DList2.get(0).equals(new Point3D(0.25,0.25,0)));
+		// ============ Equivalence Partitions Tests ==============
+		//Ray outside against vertex
+		Ray ray3 = new Ray (point6,vector1);
+		List <Point3D>point3DList3 = triangle.findIntsersections(ray3);
+		assertEquals("The test failed, the function found more than one intersection point",1,point3DList3.size(),0.1);
+		assertTrue("The test failed, the function did not find what it was looking for",point3DList3.get(0).equals(new Point3D(1.25,1.25,0)));
+		// =============== Boundary Values Tests ==================
+		//the ray begins before the plane On edge
+		Ray ray4= new Ray (point7,vector1);
+		List <Point3D>point3DList4 = triangle.findIntsersections(ray4);
+		assertEquals("The test failed, the function found more than one intersection point",1,point3DList4.size(),0.1);
+		assertTrue("The test failed, the function did not find what it was looking for",point3DList4.get(0).equals(new Point3D(0.5,0.5,0)));
+		// =============== Boundary Values Tests ==================
+		//the ray begins before the plane In vertex
+		Ray ray5= new Ray (point8,vector1);
+		List <Point3D>point3DList5 = triangle.findIntsersections(ray5);
+		assertEquals("The test failed, the function found more than one intersection point",1,point3DList5.size(),0.1);
+		assertTrue("The test failed, the function did not find what it was looking for",point3DList5.get(0).equals(new Point3D(1,1,0)));
+		// =============== Boundary Values Tests ==================
+		//the ray begins before the plane On edge's continuation
+		Ray ray6= new Ray (point9,vector1);
+		List <Point3D>point3DList6 = triangle.findIntsersections(ray6);
+		assertEquals("The test failed, the function found more than one intersection point",1,point3DList6.size(),0.1);
+		assertTrue("The test failed, the function did not find what it was looking for",point3DList6.get(0).equals(new Point3D(1,5,0)));
 	}
 
 }
